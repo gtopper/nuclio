1df54ab7dc10fe7fbde09ba55697adf50ae145b2
Gal Topper <galt@iguaz.io>
2019-07-23 10:07:47 +0300

    Revert changes for running locally.

--- pkg/processor/runtime/rpc/abstract.go
+++ pkg/processor/runtime/rpc/abstract.go
@@ -21,7 +21,6 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"github.com/rs/xid"
 	"io"
 	"net"
 	"net/http"
@@ -35,11 +34,11 @@ import (
 
 	"github.com/nuclio/logger"
 	"github.com/nuclio/nuclio-sdk-go"
+	"github.com/rs/xid"
 )
 
 // TODO: Find a better place (both on file system and configuration)
 const (
-	//socketPathTemplate = "/tmp/nuclio-rpc.sock"
 	socketPathTemplate = "/tmp/nuclio-rpc-%s.sock"
 	connectionTimeout  = 2 * time.Minute
 )
@@ -194,7 +193,6 @@ func (r *AbstractRuntime) GetSocketType() SocketType {
 
 // WaitForStart returns whether the runtime supports sending an indication that it started
 func (r *AbstractRuntime) WaitForStart() bool {
-	//return true
 	return false
 }
 
@@ -224,7 +222,6 @@ func (r *AbstractRuntime) startWrapper() error {
 		return errors.Wrap(err, "Can't run wrapper")
 	}
 	r.wrapperProcess = wrapperProcess
-	//address = address
 
 	conn, err := listener.Accept()
 	if err != nil {
@@ -238,7 +235,6 @@ func (r *AbstractRuntime) startWrapper() error {
 	go r.wrapperOutputHandler(conn, r.resultChan)
 
 	// wait for start if required to
-	//fmt.Printf("r.runtime.WaitForStart()=%+v\n", r.runtime.WaitForStart())
 	if r.runtime.WaitForStart() {
 		r.Logger.Debug("Waiting for start")
 
@@ -253,7 +249,6 @@ func (r *AbstractRuntime) startWrapper() error {
 // Create a listener on unix domian docker, return listener, path to socket and error
 func (r *AbstractRuntime) createUnixListener() (net.Listener, string, error) {
 	socketPath := fmt.Sprintf(socketPathTemplate, xid.New().String())
-	//socketPath := fmt.Sprintf(socketPathTemplate)
 
 	if common.FileExists(socketPath) {
 		if err := os.Remove(socketPath); err != nil {
